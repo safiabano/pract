37dacb99f286e1f8b58545c523863c2f8b84c1e3
safiabano <safiabano736@gmail.com>
2023-04-04 15:38:56 +0500

    practice js

---
+++ js class 1/assets/js/app.js
@@ -0,0 +1,33 @@
+//alert("Hello world");
+// var myName="mark";
+//var lol="anas";
+//document.write("Hello world");
+//document.write(2+2);
+//data types
+//string ki nishani ""hy
+//integer ya numbers me "" nhi use hongy
+//document.write("2"+"saad");
+//alert(2+2)
+//console.log("class one of js");
+//let firstName="safia bano";
+//document.write(firstName);
+//alert(firstName);
+//console.log(firstName);
+//
+//document.write(a123)
+//let firstName="safia"
+//let lastName="bano"
+//document.write(firstName+" "+lastName)
+//let num1=4
+//let num2=8
+//let num3=6
+//document.write(num3+num2-num1)
+let num1=5
+let num2=10
+let num3=15
+let num4=20
+let total =num1+num2*num3-num4
+document.write(total)
+document.write("hello"+"<br>"+"world")
+document.write("<table border=1><tr><th>heading1</th><th>heading2</th></tr></table>")
+ 
